dbebccaf306287f949018e546e045810280ef46c
Priyanshu-Chouhan <priyanshusingh99p@gmail.com>
2025-09-10 21:29:18 +0530

    chore: remove backend test files from repo

--- backend/test-env.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-console.log('Environment variables:');
-console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
-console.log('FIREBASE_CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
-console.log('FIREBASE_PRIVATE_KEY length:', process.env.FIREBASE_PRIVATE_KEY?.length);
